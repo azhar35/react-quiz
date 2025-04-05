8b391242286e36856cc5daa2e13f56c07528d73e
azhar35 <88520158+azhar35@users.noreply.github.com>
2025-04-05 11:41:39 -0400

    restarting a quiz

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,4 +1,4 @@
-function FinishScreen({ points, maxPossiblePoints, highscore }) {
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
   const percentage = (points / maxPossiblePoints) * 100;
 
   let emoji;
@@ -17,6 +17,12 @@ function FinishScreen({ points, maxPossiblePoints, highscore }) {
         {Math.ceil(percentage)})
       </p>
       <p className="highScore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: 'restart' })}
+      >
+        Restart quiz
+      </button>
     </>
   );
 }
